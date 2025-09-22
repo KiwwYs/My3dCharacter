40a42c78d4956204f5d615714eb608fd767fa1a3
KiwwYs <nattakronpoom48@gmail.com>
2025-09-22 21:09:12 +0700

    init

--- scene.gd
+++ scene.gd
@@ -1,1 +1,7 @@
 extends Node3D
+
+func _ready():
+	$boi1/AnimationPlayer.play("walking")
+	$boi2/AnimationPlayer.play("Locomotion-Library/run")
+	$boi3/AnimationPlayer.play("ShooterLib/cqb-KO-victim-back")
+	$boi4/AnimationPlayer.play("ShooterLib/punch1")
